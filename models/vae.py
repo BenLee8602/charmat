c2f4f61e1c8b29c2a9f3a18a9aba5806b80c5be4
ben <ben.lee8602@gmail.com>
2023-10-13 21:45:27 -0400

    vae constructor

--- models/vae.py
+++ models/vae.py
@@ -1,8 +1,34 @@
 from torch import nn
 
-class Vae(nn.module):
-    def __init__(self):
+class Vae(nn.Module):
+    def __init__(self,
+                 encode_dims: list[int],
+                 latent_dim: int,
+                 decode_dims: list[int] = None):
         super(Vae, self).__init__()
+        if decode_dims is None:
+            decode_dims = list(reversed(encode_dims))
+        
+        # encoder
+        in_dim = encode_dims[0]
+        encoder_layers = []
+        for d in encode_dims[1:]:
+            encoder_layers.append(nn.Linear(in_dim, d))
+            encoder_layers.append(nn.ReLU())
+            in_dim = d
+        self.encoder = nn.Sequential(*encoder_layers)
+        
+        self.get_mu = nn.Linear(encode_dims[-1], latent_dim)
+        self.get_logv = nn.Linear(encode_dims[-1], latent_dim)
+
+        # decoder
+        in_dim = latent_dim
+        decoder_layers = []
+        for d in decode_dims:
+            decoder_layers.append(nn.Linear(in_dim, d))
+            decoder_layers.append(nn.ReLU())
+            in_dim = d
+        self.decoder = nn.Sequential(*decoder_layers)
 
     def encode(self, x):
         pass
